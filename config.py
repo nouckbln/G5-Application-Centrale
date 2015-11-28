3ccd42508ebd8204b2d604089e543e2cd5932b8f
Max Halford <maxhalford25@gmail.com>
2015-11-28 18:45:19 +0100

    Initalisation

--- config.py
+++
@@ -1,9 +0,0 @@
-# Clé secrète pour générer des tokens
-SECRET_KEY = 'houdini'
-# DEBUG doit être False en production par souçi de sécurité
-DEBUG = True
-# Détail de la connection à SQLite
-SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
-SQLALCHEMY_TRACK_MODIFICATIONS = True
-# Nombre de fois qu'un mot de passe est hashé
-BCRYPT_LOG_ROUNDS = 12
